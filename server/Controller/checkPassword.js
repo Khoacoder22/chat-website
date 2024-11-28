0dfacda07488588897403bfc429a9d85b750f795
hello_khoa <118266027+Khoacoder22@users.noreply.github.com>
2024-11-28 21:37:25 +0700

    true

--- server/Controller/checkPassword.js
+++ server/Controller/checkPassword.js
@@ -25,8 +25,8 @@ async function checkPassword(request,response){
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
 
         const cookieOptions = {
-            http : true,
-            secure : true
+            httpOnly: true,
+            secure : false
         }
 
         return response.cookie('token',token,cookieOptions).status(200).json({
